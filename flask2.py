95abb39f524524c564e24d274c210b8ec561f7bf
Won Jun YANG <83794055+wonJJun2@users.noreply.github.com>
2022-12-18 18:32:30 +0900

    Create flask2.py

---
+++ flask2.py
@@ -0,0 +1,13 @@
+# fasttext로 벡터 생성 후 플라스크로 내보내기
+from flask import Flask
+app = Flask(__name__)
+@app.route('/vector/<word>')
+@cross_origin()
+
+def make_vector(keyword):
+
+    query = ft_model.wv.get_vector(word) 
+    return str(query)
+
+if __name__ == '__main__':
+    app.run(debug = False, host='0.0.0.0', port=port, threaded = True)
